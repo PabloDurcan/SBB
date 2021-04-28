013f39e4f3df6cd8c613723df73237dc0498695d
pablo <pablodurcan@gmail.com>
2021-04-28 19:41:13 +0200

    a

--- codigo.js
+++ codigo.js
@@ -1,15 +1,4 @@
-var playPromise = document.querySelector('video').play();
 
-// In browsers that don’t yet support this functionality,
-// playPromise won’t be defined.
-if (playPromise !== undefined) {
-  playPromise.then(function() {
-    // Automatic playback started!
-  }).catch(function(error) {
-    // Automatic playback failed.
-    // Show a UI element to let the user manually start playback.
-  });
-}
 
 function cargar(){
     document.getElementById("texto").style.display = "none";
